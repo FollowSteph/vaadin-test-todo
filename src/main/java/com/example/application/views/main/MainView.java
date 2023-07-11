4d9916c1e126a67ca64cc1e04b24bcf7da7f59d4
FollowSteph <steph@followsteph.com>
2023-07-11 17:35:35 -0400

    Proof of bug

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -2,11 +2,14 @@ package com.example.application.views.main;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.List;
 
 @PageTitle("Main")
 @Route(value = "")
@@ -15,18 +18,33 @@ public class MainView extends HorizontalLayout {
     private TextField name;
     private Button sayHello;
 
+    private ComboBox<Person> personComboBox;
+
     public MainView() {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+//            Notification.show("Hello " + name.getValue());
+            Notification.show("Hello " + (personComboBox.getValue() == null ? "Null" : personComboBox.getValue().getName()));
         });
         sayHello.addClickShortcut(Key.ENTER);
+
+        List<Person> people = generateListOfPeople();
+        personComboBox = new ComboBox<>("Select person", people);
+        personComboBox.addValueChangeListener(event -> System.out.println("Selected person: " + event.getValue()));
 
         setMargin(true);
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
         add(name, sayHello);
+        add(personComboBox);
     }
 
+    private List<Person> generateListOfPeople() {
+        return List.of(
+                new Person("John"),
+                new Person("Jane"),
+                new Person(""),
+                new Person(""));
+    }
 }
